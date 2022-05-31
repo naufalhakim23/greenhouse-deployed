f5d718aaf766031ba4959d735165e0da79564c14
rpo <hakimnaufal212@gmail.com>
2022-05-31 17:11:05 +0700

    Feat: reading every feature mapped to Graph.jsx

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -41,7 +41,11 @@ function MainDashboard() {
         week: 0,
     });
 
-    // const [twentyDataFromLast, setTwentyDataFromLast] = useState([]);
+    const [twentyTimestamp, settwentyTimestamp] = useState(null);
+    const [twentyRoomHumidity, setTwentyRoomHumidity] = useState(null);
+    const [twentyRoomTemperature, setTwentyRoomTemperature] = useState(null);
+    const [twentyPercentageSoilSensor1, setTwentyPercentageSoilSensor1] = useState(null);
+    const [twentyPercentageSoilSensor2, setTwentyPercentageSoilSensor2] = useState(null);
 
     const theme = useMantineTheme();
     // const [opened, setOpened] = useState(false);
@@ -70,20 +74,45 @@ function MainDashboard() {
         return output;
     }
     // belom di handle
-    // const getDataForGraph20 = () => {
-    //     const dataRTDB = ref(db, 'node/database/')
-    //     const lastDataRTDBtoTwenty = query(dataRTDB, limitToLast(20))
-    //     onValue(lastDataRTDBtoTwenty, (snapshot) => {
-    //         const data = snapshot.val()
-
-    //         setTwentyDataFromLast(data)
-    //     }
-    //     )
-    // }
+    const getDataForGraph20 = () => {
+        const dataRTDB = ref(db, 'node/database/')
+        const lastDataRTDBtoTwenty = query(dataRTDB, limitToLast(20))
+        onValue(lastDataRTDBtoTwenty, (snapshot) => {
+            const data = snapshot.val()
+            var item = Object.keys(data).map(function (key) {
+                return data[key];
+            });
+            // get only roomTemperature from data
+            var roomTemperature = item.map(function (item) {
+                return item?.roomTemperature;
+            });
+            // get only roomHumidity from data
+            var roomHumidity = item.map(function (item) {
+                return item?.roomHumidity;
+            });
+            // get only soil moisture percentage from data
+            var percentageSoilSensor1 = item.map(function (item) {
+                return item?.percentageSoilSensor1;
+            });
+            var percentageSoilSensor2 = item.map(function (item) {
+                return item?.percentageSoilSensor2;
+            });
+            // get only timestamp from data
+            var timestamp = item.map(function (item) {
+                return item?.Ts;
+            });
+            settwentyTimestamp(timestamp)
+            setTwentyRoomHumidity(roomHumidity)
+            setTwentyRoomTemperature(roomTemperature)
+            setTwentyPercentageSoilSensor1(percentageSoilSensor1)
+            setTwentyPercentageSoilSensor2(percentageSoilSensor2)
+        }
+        )
+    }
     useEffect(() => {
         getDataInterfaceFirebase();
         getDataComparator();
-        // getDataForGraph20()
+        getDataForGraph20()
     }, [])
     return (
         <>
@@ -117,9 +146,6 @@ function MainDashboard() {
                     }
                 ]} />}
             >
-                {/* <pre>{JSON.stringify(dataInterface)} test</pre>
-                <pre>{JSON.stringify(dataComparator)} test</pre>
-                <pre>{JSON.stringify(twentyDataFromLast)}</pre> */}
                 <StatsGrid data={[
                     {
                         "title": "Plant 1 Soil Moisture Percentage",
@@ -172,14 +198,30 @@ function MainDashboard() {
                 ]} />
                 <Grid>
                     <Grid.Col xs={7} >
-                        <CardGraph />
+                        <CardGraph 
+                        dataFromDatabase={twentyRoomTemperature}
+                        dataTimestamp={twentyTimestamp}
+                         />
                     </Grid.Col>
-                    <Grid.Col xs={5}>{child}</Grid.Col>
-                    <Grid.Col xs={8}>{child}</Grid.Col>
-                    <Grid.Col xs={4}>{child}</Grid.Col>
-                    <Grid.Col xs={3}>{child}</Grid.Col>
-                    <Grid.Col xs={3}>{child}</Grid.Col>
-                    <Grid.Col xs={6}>{child}</Grid.Col>
+                    <Grid.Col xs={7}>
+                        <CardGraph
+                        dataFromDatabase={twentyRoomHumidity}
+                        dataTimestamp={twentyTimestamp}
+                        />
+                    </Grid.Col>
+                    <Grid.Col xs={7}>
+                        <CardGraph
+                        dataFromDatabase={twentyPercentageSoilSensor1}
+                        dataTimestamp={twentyTimestamp}
+                        />
+                    </Grid.Col>
+                    <Grid.Col xs={7}>
+                        <CardGraph
+                        dataFromDatabase={twentyPercentageSoilSensor2}
+                        dataTimestamp={twentyTimestamp}
+                        />
+                    </Grid.Col>
+
                 </Grid>
             </AppShell>
         </>
