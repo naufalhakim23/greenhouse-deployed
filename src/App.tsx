a07760ac7a5bd0d1f0aaa2397eda7c2acdb56676
rpo <hakimnaufal212@gmail.com>
2022-06-04 22:14:45 +0700

    Feat: Adding Login dummy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import MainDashboard from './Pages/Index';
 import { Routes, Route } from 'react-router-dom';
 import GraphPage from './Pages/GraphPage';
+import { Authentication } from './Pages/Login';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<MainDashboard />} />
         <Route path="/graph-view" element={<GraphPage />} />
+        <Route path="/settings" element={<div>Settings</div>} />
+        <Route path="/login" element={<Authentication/>} />
       </Routes>
     </div>
   );
