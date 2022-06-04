58f0a7975a438ffc31e7d4f2885a0b44a31d94f2
rpo <hakimnaufal212@gmail.com>
2022-06-04 22:15:21 +0700

    Feat: Moving graph feature from Index to Graph Page

--- src/Pages/GraphPage.jsx
+++ src/Pages/GraphPage.jsx
@@ -1,16 +1,56 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { AppShell, Grid } from '@mantine/core'
 // // components
+import CardGraph from "../components/Graph";
 import { HeaderSearch } from "../components/Header";
 import { NavbarMinimal } from "../components/Navbar";
+// // interaction with the API
+import { db } from "../Firebase/Firebase";
+import { ref, onValue, limitToLast, query } from "firebase/database";
 
 export default function GraphPage() {
-    // const [twentyTimestamp, settwentyTimestamp] = useState(null);
-    // const [twentyRoomHumidity, setTwentyRoomHumidity] = useState(null);
-    // const [twentyRoomTemperature, setTwentyRoomTemperature] = useState(null);
-    // const [twentyPercentageSoilSensor1, setTwentyPercentageSoilSensor1] = useState(null);
-    // const [twentyPercentageSoilSensor2, setTwentyPercentageSoilSensor2] = useState(null);
-    useEffect(() => { })
+    const [twentyTimestamp, settwentyTimestamp] = useState(null);
+    const [twentyRoomHumidity, setTwentyRoomHumidity] = useState(null);
+    const [twentyRoomTemperature, setTwentyRoomTemperature] = useState(null);
+    const [twentyPercentageSoilSensor1, setTwentyPercentageSoilSensor1] = useState(null);
+    const [twentyPercentageSoilSensor2, setTwentyPercentageSoilSensor2] = useState(null);
+    const getDataForGraph20 = () => {
+        const dataRTDB = ref(db, "node/database/");
+        const lastDataRTDBtoTwenty = query(dataRTDB, limitToLast(20));
+        onValue(lastDataRTDBtoTwenty, (snapshot) => {
+            const data = snapshot.val();
+            var item = Object.keys(data).map(function (key) {
+                return data[key];
+            });
+            // get only roomTemperature from data
+            var roomTemperature = item.map(function (item) {
+                return item?.roomTemperature;
+            });
+            // get only roomHumidity from data
+            var roomHumidity = item.map(function (item) {
+                return item?.roomHumidity;
+            });
+            // get only soil moisture percentage from data
+            var percentageSoilSensor1 = item.map(function (item) {
+                return item?.percentageSoilSensor1;
+            });
+            var percentageSoilSensor2 = item.map(function (item) {
+                return item?.percentageSoilSensor2;
+            });
+            // get only timestamp from data
+            var timestamp = item.map(function (item) {
+                return item?.Ts;
+            });
+            settwentyTimestamp(timestamp);
+            setTwentyRoomHumidity(roomHumidity);
+            setTwentyRoomTemperature(roomTemperature);
+            setTwentyPercentageSoilSensor1(percentageSoilSensor1);
+            setTwentyPercentageSoilSensor2(percentageSoilSensor2);
+        });
+    };
+    useEffect(() => {
+        getDataForGraph20();
+    }, []);
     return (
         <>
             <AppShell
@@ -20,36 +60,36 @@ export default function GraphPage() {
                 asideOffsetBreakpoint="sm"
                 fixed
                 navbar={
-                    <NavbarMinimal />
+                    <NavbarMinimal
+                    />
                 }
                 header={<HeaderSearch />}
             >
                 <Grid>
-                    {/* <Grid.Col xs={7} >
-                        <CardGraph 
-                        dataFromDatabase={twentyRoomTemperatureValue}
-                        dataTimestamp={twentyTimestampValue}
-                         />
-                    </Grid.Col>
-                    <Grid.Col xs={7}>
+                    <Grid.Col xs={6}>
                         <CardGraph
-                        dataFromDatabase={twentyRoomHumidityValue}
-                        dataTimestamp={twentyTimestampValue}
+                            dataFromDatabase={twentyRoomTemperature}
+                            dataTimestamp={twentyTimestamp}
                         />
                     </Grid.Col>
-                    <Grid.Col xs={7}>
+                    <Grid.Col xs={6}>
                         <CardGraph
-                        dataFromDatabase={twentyPercentageSoilSensor1Value}
-                        dataTimestamp={twentyTimestampValue}
+                            dataFromDatabase={twentyRoomHumidity}
+                            dataTimestamp={twentyTimestamp}
                         />
                     </Grid.Col>
-                    <Grid.Col xs={7}>
+                    <Grid.Col xs={6}>
                         <CardGraph
-                        dataFromDatabase={twentyPercentageSoilSensor2Value}
-                        dataTimestamp={twentyTimestampValue}
+                            dataFromDatabase={twentyPercentageSoilSensor1}
+                            dataTimestamp={twentyTimestamp}
                         />
-                    </Grid.Col> */}
-
+                    </Grid.Col>
+                    <Grid.Col xs={6}>
+                        <CardGraph
+                            dataFromDatabase={twentyPercentageSoilSensor2}
+                            dataTimestamp={twentyTimestamp}
+                        />
+                    </Grid.Col>
                 </Grid>
 
 
--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,19 +1,14 @@
 import { useState, useEffect } from "react";
-import { AppShell, useMantineTheme, Grid } from "@mantine/core";
+import { AppShell, useMantineTheme } from "@mantine/core";
 import { HeaderSearch } from "../components/Header";
 import { NavbarMinimal } from "../components/Navbar";
 import { StatsGrid } from "../components/Status";
-import CardGraph from "../components/Graph";
 // interaction with the API
 import { db } from "../Firebase/Firebase";
 import { ref, onValue, limitToLast, query } from "firebase/database";
-// import { useStore } from "zustand";
-// react-router-dom
 
 function MainDashboard() {
     const [dataInterface, setDataInterface] = useState({
-        bitSoilSensor1: 0,
-        bitSoilSensor2: 0,
         percentageSoilSensor1: 0,
         percentageSoilSensor2: 0,
         roomHumidity: 0,
@@ -23,8 +18,6 @@ function MainDashboard() {
         Ts: 0,
     });
     const [dataComparator, setDataComparator] = useState({
-        bitSoilSensor1: 0,
-        bitSoilSensor2: 0,
         percentageSoilSensor1: 0,
         percentageSoilSensor2: 0,
         roomHumidity: 0,
@@ -32,14 +25,7 @@ function MainDashboard() {
         status: 0,
         week: 0,
     });
-    // const {} = useStore()
-    const [twentyTimestamp, settwentyTimestamp] = useState(null);
-    const [twentyRoomHumidity, setTwentyRoomHumidity] = useState(null);
-    const [twentyRoomTemperature, setTwentyRoomTemperature] = useState(null);
-    const [twentyPercentageSoilSensor1, setTwentyPercentageSoilSensor1] =
-        useState(null);
-    const [twentyPercentageSoilSensor2, setTwentyPercentageSoilSensor2] =
-        useState(null);
+
 
     const theme = useMantineTheme();
 
@@ -64,45 +50,9 @@ function MainDashboard() {
             }/${date.getFullYear()} GMT+7`;
         return output;
     };
-    // belom di handle
-    const getDataForGraph20 = () => {
-        const dataRTDB = ref(db, "node/database/");
-        const lastDataRTDBtoTwenty = query(dataRTDB, limitToLast(20));
-        onValue(lastDataRTDBtoTwenty, (snapshot) => {
-            const data = snapshot.val();
-            var item = Object.keys(data).map(function (key) {
-                return data[key];
-            });
-            // get only roomTemperature from data
-            var roomTemperature = item.map(function (item) {
-                return item?.roomTemperature;
-            });
-            // get only roomHumidity from data
-            var roomHumidity = item.map(function (item) {
-                return item?.roomHumidity;
-            });
-            // get only soil moisture percentage from data
-            var percentageSoilSensor1 = item.map(function (item) {
-                return item?.percentageSoilSensor1;
-            });
-            var percentageSoilSensor2 = item.map(function (item) {
-                return item?.percentageSoilSensor2;
-            });
-            // get only timestamp from data
-            var timestamp = item.map(function (item) {
-                return item?.Ts;
-            });
-            settwentyTimestamp(timestamp);
-            setTwentyRoomHumidity(roomHumidity);
-            setTwentyRoomTemperature(roomTemperature);
-            setTwentyPercentageSoilSensor1(percentageSoilSensor1);
-            setTwentyPercentageSoilSensor2(percentageSoilSensor2);
-        });
-    };
     useEffect(() => {
         getDataInterfaceFirebase();
         getDataComparator();
-        getDataForGraph20();
     }, []);
     return (
         <>
@@ -128,56 +78,40 @@ function MainDashboard() {
                             icon: "receipt",
                             value: `${dataInterface.percentageSoilSensor1}%`,
                             diff:
-                                parseInt(`${dataComparator.percentageSoilSensor1}`) -
-                                parseInt(`${dataInterface.percentageSoilSensor1}`),
-                        },
-                        {
-                            title: "Plant 1 Soil Moisture BitOutput",
-                            icon: "coin",
-                            value: `${dataInterface.bitSoilSensor1}`,
-                            diff:
-                                parseInt(`${dataComparator.bitSoilSensor1}`) -
-                                parseInt(`${dataInterface.bitSoilSensor1}`),
+                                parseInt(`${dataInterface.percentageSoilSensor1}`) -
+                                parseInt(`${dataComparator.percentageSoilSensor1}`)
                         },
                         {
                             title: "Plant 2 Soil Moisture Percentage",
                             icon: "discount",
                             value: `${dataInterface.percentageSoilSensor2}`,
                             diff:
-                                parseInt(`${dataComparator.percentageSoilSensor2}`) -
-                                parseInt(`${dataInterface.percentageSoilSensor2}`),
-                        },
-                        {
-                            title: "Plant 2 Soil Moisture BitOutput",
-                            icon: "user",
-                            value: `${dataInterface.bitSoilSensor2}`,
-                            diff:
-                                parseInt(`${dataComparator.bitSoilSensor2}`) -
-                                parseInt(`${dataInterface.bitSoilSensor2}`),
+                                parseInt(`${dataInterface.percentageSoilSensor2}`) -
+                                parseInt(`${dataComparator.percentageSoilSensor2}`)
                         },
                         {
                             title: "Greenery Humidity",
                             icon: "user",
                             value: `${dataInterface.roomHumidity}`,
                             diff:
-                                parseInt(`${dataComparator.roomHumidity}`) -
-                                parseInt(`${dataInterface.roomHumidity}`),
+                                parseInt(`${dataInterface.roomHumidity}`) -
+                                parseInt(`${dataComparator.roomHumidity}`)
                         },
                         {
                             title: "Greenery Temperature",
                             icon: "temperature",
                             value: `${dataInterface.roomTemperature}`,
                             diff:
-                                parseInt(`${dataComparator.roomTemperature}`) -
-                                parseInt(`${dataInterface.roomTemperature}`),
+                                parseInt(`${dataInterface.roomTemperature}`) -
+                                parseInt(`${dataComparator.roomTemperature}`)
                         },
                         {
                             title: "Week",
                             icon: "temperature",
                             value: `${dataInterface.week}`,
                             diff:
-                                parseInt(`${dataComparator.week}`) -
-                                parseInt(`${dataInterface.week}`),
+                                parseInt(`${dataInterface.week}`) -
+                                parseInt(`${dataComparator.week}`)
                         },
                         {
                             title: "Time Last Retrieve",
@@ -187,32 +121,6 @@ function MainDashboard() {
                         },
                     ]}
                 />
-                <Grid>
-                    <Grid.Col xs={7}>
-                        <CardGraph
-                            dataFromDatabase={twentyRoomTemperature}
-                            dataTimestamp={twentyTimestamp}
-                        />
-                    </Grid.Col>
-                    <Grid.Col xs={7}>
-                        <CardGraph
-                            dataFromDatabase={twentyRoomHumidity}
-                            dataTimestamp={twentyTimestamp}
-                        />
-                    </Grid.Col>
-                    <Grid.Col xs={7}>
-                        <CardGraph
-                            dataFromDatabase={twentyPercentageSoilSensor1}
-                            dataTimestamp={twentyTimestamp}
-                        />
-                    </Grid.Col>
-                    <Grid.Col xs={7}>
-                        <CardGraph
-                            dataFromDatabase={twentyPercentageSoilSensor2}
-                            dataTimestamp={twentyTimestamp}
-                        />
-                    </Grid.Col>
-                </Grid>
             </AppShell>
         </>
     );
